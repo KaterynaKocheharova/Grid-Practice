1f20aa2f39271745465cb67982804eee377f933d
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-29 23:50:00 +0300

    Add base styles

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,11 @@
+import css from "./Container.module.css";
+
+const Container = () => {
+  return (
+    <div className={css.container}>
+      CONTAINER
+    </div>
+  )
+}
+
+export default Container
