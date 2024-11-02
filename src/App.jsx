618110eba31ed3d9c4776712ad244498d65dd2a1
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-03 01:45:13 +0300

    pratice grid-templat-col and row

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import css from "./components/Container/Container.module.css";
 export default function App() {
   return (
     <div id="App">
-      <div className={css.container}>GRID CONTAINER</div>
-      <div className={css.container}>GRID CONTAINER</div>
+      <div className={css.container}>
+        <div className="grid-item">1</div>
+        <div className="grid-item">2</div>
+        <div className="grid-item">3</div>
+        <div className="grid-item">4</div>
+        <div className="grid-item">5</div>
+        <div className="grid-item">6</div>
+        <div className="grid-item">7</div>
+        <div className="grid-item">8</div>
+        <div className="grid-item">9</div>
+        <div className="grid-item">10</div>
+      </div>
+
     </div>
   );
 }
